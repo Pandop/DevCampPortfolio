0bc0c4fd43f64f2714d9caf33e5e648af80265b5
Pandop <s.kel@student.qut.edu.au>
2018-06-28 22:44:33 +1000

    Create seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: " My Blog Post #{blog}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore a fugiat recusandae quas nam provident, officia cumque autem voluptatibus quis modi iusto similique."
+  )
+end
+puts "10 blog posts created."
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_completed: 15
+  )
+end
+puts "5 skill posts created."
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My great service",
+    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.  Labore a fugiat recusandae quas nam provident, officia cumque autem voluptatibus quis modi iusto similique. Eaque perspiciatis, asperiores possimus aperiam mollitia accusantium? 
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore a fugiat recusandae quas nam provident, officia cumque autem voluptatibus quis modi iusto similique. Eaque perspiciatis, asperiores possimus aperiam mollitia accusantium?",
+    main_image: "http://placeholder.it/600x400",
+    thumb_image: "http://placeholder.it/350x200"
+  )
+end
+puts "9 portfolio posts created."
