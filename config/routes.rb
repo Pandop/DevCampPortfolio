52b66619ba60bf650adf51860995592f548d0837
Pandop <s.kel@student.qut.edu.au>
2018-06-29 15:13:16 +1000

    Add custom routes for controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-    
-  root 'blogs#index'
-  
   resources :portfolios
-  resources :blogs
 
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about-me',to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  resources :blogs    
+  root 'pages#home'
+  
+ # get 'pages/home'
+ # get 'pages/about'
+ # get 'pages/contact'
 end
