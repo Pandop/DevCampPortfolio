39e650a393b02828d58b0dc7a83c7f4d78627d90
Pandop <s.kel@student.qut.edu.au>
2018-06-30 15:44:18 +1000

    Add data defaults for images for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,6 +6,8 @@ class Portfolio < ApplicationRecord
 
   validates :main_image, :thumb_image, presence: true
 
+  after_initialize :set_defaults
+
   # scope :angular, ->{where(subtitle: "Angular")}
   # scope :sorted,  ->{order("created_at DESC")}
   # scope :ruby_on_rails_portfolio_items, ->{where(subtitle: "Ruby on Rails")}
@@ -21,6 +23,18 @@ class Portfolio < ApplicationRecord
 
   def self.sorted 
     order("created_at DESC")
+  end
+
+  def set_defaults
+    self.main_image  ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200" 
+
+   ## if self.main_image == nil 
+      #self.main_image = "http://via.placeholder.com/600x400"
+    #end
+  ##  if
+    # self.thumb_image ||= "http://via.placeholder.com/350x200"
+    #end
   end
 
 end
