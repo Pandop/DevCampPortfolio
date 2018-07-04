35ab8a5efbb86d31e3e6f62b5a6589e911fda47f
Pandop <s.kel@student.qut.edu.au>
2018-07-04 15:49:33 +1000

    Refactor application controller to include source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,8 @@
 class ApplicationController < ActionController::Base
-  #protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   include DeviseBlacklist
 
-  before_action :set_source 
-
-  def set_source 
-    return session[:source]= params[:q] if params[:q]
-  end
+  include SetSource
 
 end
