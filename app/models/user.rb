b809b21815da37af0e0380e0cd0a052021a4c32b
Pandop <s.kel@student.qut.edu.au>
2018-07-04 14:29:08 +1000

    Complete version authentication for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,   :registerable,
          :recoverable, :rememberable, :trackable,   :validatable
+
+  validates :name, presence:true 
+
+  def first_name
+    return self.name.split.first 
+  end
+
+  def last_name
+    return self.name.split.last
+  end
+
 end
