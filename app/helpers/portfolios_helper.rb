41a68ae4dbdcfc97fcd026af526c011832ddf956
Pandop <s.kel@student.qut.edu.au>
2018-07-12 17:12:16 +1000

    Update file upload forms

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,32 @@
 module PortfoliosHelper
+
+  def generate_image(height:, width:) 
+    return "http://via.placeholder.com/#{height}x#{width}"
+  end
+
+  def portfolio_img(img, type)
+    #byebug
+    if img.model.main_image? || img.model.thumb_image? 
+      return img 
+    elsif type=='thumb'
+      generate_image(height:'356', width:'280')
+    elsif type=='main'
+      generate_image(height:'600', width:'400')
+    end
+  end
+
+  def set_defaults
+    self.main_image  ||= generate_image(600, 400)
+    self.thumb_image ||= generate_image(356, 280)
+
+    #self.main_image  ||= "http://via.placeholder.com/600x400"
+    #self.thumb_image ||= "http://via.placeholder.com/350x200" 
+
+   ## if self.main_image == nil 
+      #self.main_image = "http://via.placeholder.com/600x400"
+    #end
+  ##  if
+    # self.thumb_image ||= "http://via.placeholder.com/350x200"
+    #end
+  end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,16 +8,19 @@ class Portfolio < ApplicationRecord
 
   validates :body, presence: true
 
-  validates :main_image, :thumb_image, presence: true
+  #validates :main_image, :thumb_image, presence: true
 
-  after_initialize :set_defaults  
+  #after_initialize :set_defaults  
 
-  include Placeholder
+  #include Placeholder
 
   # scope :angular, ->{where(subtitle: "Angular")}
   # scope :sorted,  ->{order("created_at DESC")}
   # scope :ruby_on_rails_portfolio_items, ->{where(subtitle: "Ruby on Rails")}
   # Same as below:
+
+  mount_uploader :main_image, PortfolioUploader
+  mount_uploader :thumb_image, PortfolioUploader
 
   def self.angular
     where(subtitle: "Angular")
@@ -35,18 +38,4 @@ class Portfolio < ApplicationRecord
     order("position ASC")
   end
 
-  def set_defaults
-    self.main_image  ||= Placeholder.generate_image(600, 400)
-    self.thumb_image ||= Placeholder.generate_image(356, 280)
-
-    #self.main_image  ||= "http://via.placeholder.com/600x400"
-    #self.thumb_image ||= "http://via.placeholder.com/350x200" 
-
-   ## if self.main_image == nil 
-      #self.main_image = "http://via.placeholder.com/600x400"
-    #end
-  ##  if
-    # self.thumb_image ||= "http://via.placeholder.com/350x200"
-    #end
-  end
 end
