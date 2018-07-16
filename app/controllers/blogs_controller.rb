9bbe8b154c47e76c4e1139afefb3632177126f80
Pandop <s.kel@student.qut.edu.au>
2018-07-16 12:17:33 +1000

    Build out initial form and partial call for comment and controller action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,6 +18,8 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @blog         = Blog.includes(:comments).friendly.find(params[:id])
+    @comment      = Comment.new   # Give empty comment
     @page_title   = @blog.title
     @seo_keywords = @blog.body
   end
